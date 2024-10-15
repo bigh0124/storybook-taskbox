5a25e0714e89f8b2fe732fe21adb889b9931214a
bigh0124 <thubgi0629@gmail.com>
2024-10-15 20:55:21 +0800

    add longtext test

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -20,7 +20,7 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
           name="title"
           id={`title-${id}`}
           placeholder="Input title"
-          style={{ backgroundColor: "red" }}
+          style={{ textOverflow: "ellipsis" }}
         />
       </label>
 
